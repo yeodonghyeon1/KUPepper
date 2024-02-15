a37b512338bc26f8168a34b401d908c6cff328a0
yeodonghyeon1 <106720684+yeodonghyeon1@users.noreply.github.com>
2024-02-15 13:26:58 +0900

    chatupdate

--- pepper/test2.py
+++ pepper/test2.py
@@ -21,10 +21,11 @@ robot.say("HI HIHIHIHIHIHIHI")
 # robot.do_hand_shake()
 # a.run()
 # robot.show_web("https://www.google.com/")
-
-robot.show_web("http://192.168.0.82:80/")
-
-
+# robot.pick_a_volunteer()
+# robot.show_web("http://192.168.0.82:80/")
+# robot.sound_detect_service.setParameter("Sensitivity", 1.0)
+# robot.listen("en-US")
+robot.chatbot()
 #test sonar
 # print(robot.memory_service.getData("Device/SubDeviceList/Platform/Front/Sonar/Sensor/Value"))
 # print(robot.memory_service.getData("Device/SubDeviceList/Platform/Front/Sonar/Sensor/Value"))
@@ -38,8 +39,8 @@ robot.show_web("http://192.168.0.82:80/")
 
 # explore
 # robot.set_security_distance(0.15)
-robot.exploration_mode(20)
-robot.show_map(on_robot=True)
+# robot.exploration_mode(20)
+# robot.show_map(on_robot=True)
 
 #robot.robot_localization()
 
